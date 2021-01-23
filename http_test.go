5bf9edc5e94075924be2d0f0aaea30af8dc90ce7
pankajsharmazopsmart <pankaj.sharma@zopsmart.com>
2021-01-23 16:46:16 +0530

    added update

--- http_test.go
+++ http_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"github.com/DATA-DOG/go-sqlmock"
 	"log"
+	"reflect"
 	"testing"
 )
 
@@ -71,13 +73,43 @@ func TestReadById(t *testing.T) {
 	}
 }
 
-//func TestUpdateFunc(t *testing.T){
-//	fdb,mock,err:=sqlmock.New()
-//	if err != nil {
-//		t.Fatalf("Can't connect to a mock database")
-//	}
-//	dbh:=DatabaseHandler{fdb}
-//	db:=dbh.Db
-//
-//
-//}
+func TestUpdateFunc(t *testing.T){
+	fdb,mock,err:=sqlmock.New()
+	if err != nil {
+		t.Fatalf("Can't connect to a mock database")
+	}
+	dbh:=DatabaseHandler{fdb}
+	db:=dbh.Db
+	defer db.Close()
+	testCase:=[]struct{
+		input string
+		output Employee
+		expectedRetrunvalue  int64
+		err error
+	}{
+		{
+			"1",Employee{"1", "Pankaj Sharma", 22, "M", "1"},1,nil,
+		},
+		{
+			"2",Employee{"2", "Rudra Bhardwaj", 21, "M", "2"},0,errors.New("User enterted the existing same data"),
+		},
+		{
+			"3",Employee{"3", "Vivek Sharma", 26, "M", "3"},1,nil,
+		},
+	}
+	//str:=[]string{"Id","Name","Age","Gender","Role"}
+	for i,tc:= range testCase {
+		mock.ExpectExec("Update Employee*").WithArgs(tc.output.Name,tc.output.Age,tc.output.Gender,tc.output.Role,tc.input).WillReturnResult(sqlmock.NewResult(0,tc.expectedRetrunvalue))
+		result,err:=dbh.UpdateData(tc.input,tc.output)
+		//if err!=nil && tc.err!=nil && err.Error() != tc.err.Error() {
+		if !reflect.DeepEqual(err,tc.err){
+			t.Fatalf("Failed at %v\nExpected Error : %v\nActual Error : %v\n", i+1, tc.err.Error(), err.Error())
+			//t.Logf("%v\n%v\n%v\n%T\n%T",i+1,err,tc.err,err,tc.err)
+		}
+		if result != tc.output {
+			t.Fatalf("Failed at %v\nExpected Output : %v\nActual Output : %v\n", i+1, tc.output, result)
+		}
+		t.Logf("Passed at %v\n", i+1)
+	}
+
+}
